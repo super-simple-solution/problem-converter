351791cded0b95da5d52b6026ae50f647cc2b202
wudaxue <wudaxue123@gmail.com>
2022-02-03 00:21:10 +0800

    leetcode solution

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,7 @@
-<script setup></script>
+<script setup>
+import Leetcode from './views/leetcode/index.vue'
+</script>
+
+<template>
+  <leetcode></leetcode>
+</template>
---
+++ src/views/leetcode/index.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, watch } from 'vue'
+import { parseContent } from './index.js'
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import json from './data/data.json'
+import json1 from './data/data1.json'
+import json2 from './data/data2.json'
+import json3 from './data/data3.json'
+import json4 from './data/data4.json'
+
+const jsonList = [json, json1, json2, json3, json4]
+const index = ref(4)
+let content = jsonList[index.value].content
+let contentRes = ref(parseContent(content))
+
+watch(index, (val) => {
+  console.log(val, 'val')
+  content = jsonList[val]?.content
+  if (!content) return
+  contentRes.value = parseContent(content)
+})
+</script>
+
+<template>
+  <div style="width: 800px">
+    <input v-model.number="index" />
+    <div v-html="contentRes"></div>
+  </div>
+</template>
